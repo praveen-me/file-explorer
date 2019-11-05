7290b4824e6f08f6fdebd61393b6aa11ce9cca4d
praveen-me <praveenkrsaini0@gmail.com>
2019-11-05 14:33:03 +0530

    Add normal UI for Files Panel

--- src/components/MainPanel/index.js
+++ src/components/MainPanel/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TopBar from "../TopBar";
+import FilesPanel from "../FilesPanel";
 
 const MainPanel = () => {
   return (
     <div className="main-panel">
       <TopBar />
+      <FilesPanel />
     </div>
   );
 };
