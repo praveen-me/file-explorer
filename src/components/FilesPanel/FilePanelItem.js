8047a5a014a98a26417bffff73856c38b185adcf
praveen-me <praveenkrsaini0@gmail.com>
2019-11-05 15:18:20 +0530

    Add separate componet for File Panel Ite

---
+++ src/components/FilesPanel/FilePanelItem.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import * as File from "./../../images/file.png";
+import * as Folder from "./../../images/folder.png";
+
+const FilePanelItem = ({ type, title }) => {
+  console.log(type, title);
+
+  const Img = <img src={type === "file" ? File : Folder} alt={type} />;
+
+  return (
+    <a href="#" className={`files-panel__item ${type}`}>
+      {type === "file" ? (
+        <div className="files-panel__item-extension">
+          {Img}
+          <span className="files-panel__item-extension-name">
+            {title.split(".")[1]}
+          </span>
+        </div>
+      ) : (
+        <>{Img}</>
+      )}
+
+      <p className="files-panel__item-title">{title}</p>
+    </a>
+  );
+};
+
+export default FilePanelItem;
