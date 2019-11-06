572840dd5baa01fb6d9226facd3790df16cfaabd
praveen-me <praveenkrsaini0@gmail.com>
2019-11-07 02:15:23 +0530

    Create functionality of opening info and open folder from options

--- src/components/FilesPanel/FilePanelItem.js
+++ src/components/FilesPanel/FilePanelItem.js
@@ -1,31 +1,62 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import * as File from "./../../images/file.png";
-import * as Folder from "./../../images/folder.png";
 import { updatePath } from "../../store/actions/common.actions";
 import PanelItemInfo from "../PopUps/PanelItemInfo";
 import PanelImage from "../utils/PanelImge";
+import OptionPopup from "../PopUps/OptionPopup";
 
 const FilePanelItem = ({ type, title, slug }) => {
+  const [isOptionsModalOpen, setIsModalOptionsOpen] = useState(false);
+  const [isItemInfoModalOpen, setIsItemInfoModalOpen] = useState(false);
+
   const dispatch = useDispatch();
 
   const handleDirectoryClick = () => {
+    if (type === "file") return;
     if (slug) {
       dispatch(updatePath(slug));
     }
     return;
   };
 
+  const handleRightClick = e => {
+    e.preventDefault();
+    if (e.type === "click") {
+      setIsModalOptionsOpen(false);
+      return;
+    }
+    setIsModalOptionsOpen(true);
+  };
+
+  const togglePanelInfoModal = () =>
+    setIsItemInfoModalOpen(!isItemInfoModalOpen);
+
   return (
-    <button
+    <div
+      style={{ position: isOptionsModalOpen ? "relative" : "static" }}
       className={`files-panel__item ${type}`}
-      onDoubleClick={handleDirectoryClick}
-      disabled={type === "file" ? true : false}
     >
-      <PanelImage type={type} title={title} />
-      <p className="files-panel__item-title">{title}</p>
-    </button>
+      <button
+        className="btn"
+        onDoubleClick={handleDirectoryClick}
+        onContextMenu={handleRightClick}
+        onClick={handleRightClick}
+      >
+        <PanelImage type={type} title={title} />
+        <p className="files-panel__item-title">{title}</p>
+      </button>
+      {isOptionsModalOpen && (
+        <OptionPopup
+          toggleInfo={togglePanelInfoModal}
+          isInfoModalOpen={isItemInfoModalOpen}
+          open={handleDirectoryClick}
+        />
+      )}
+      {isItemInfoModalOpen && (
+        <PanelItemInfo toggleInfo={togglePanelInfoModal} />
+      )}
+    </div>
   );
 };
 
--- src/components/PopUps/PanelItemInfo.js
+++ src/components/PopUps/PanelItemInfo.js
@@ -1,24 +1,26 @@
 import React from "react";
-import * as Folder from "./../../images/folder.png";
-import * as File from "./../../images/file.png";
 import CloseBtn from "../utils/CloseBtn";
 import PanelImage from "../utils/PanelImge";
 import InfoPopupDetailItem from "./InfoPopupDetailItem";
 
-const PanelItemInfo = () => {
+const PanelItemInfo = ({ toggleInfo }) => {
   return (
-    <div className="panel-item-info modal">
-      <div className="modal__head-container">
-        <h3 className="modal-head__head">File Info</h3>
-        <CloseBtn />
-      </div>
-      <div className="panel-item-info_wrapper">
-        <PanelImage type="folder" />
-      </div>
-      <div className="panel-item-info__details">
-        <InfoPopupDetailItem keyName="Name" value="index.html" />
-        <InfoPopupDetailItem keyName="Size" value="362kb" />
-        <InfoPopupDetailItem keyName="Author" value="Praveen" />
+    <div className="overlay">
+      <div className="panel-item-info modal">
+        <div className="modal__head-container">
+          <h3 className="modal-head__head">File Info</h3>
+          <button className="btn" onClick={toggleInfo}>
+            <CloseBtn />
+          </button>
+        </div>
+        <div className="panel-item-info_wrapper">
+          <PanelImage type="folder" />
+        </div>
+        <div className="panel-item-info__details">
+          <InfoPopupDetailItem keyName="Name" value="index.html" />
+          <InfoPopupDetailItem keyName="Size" value="362kb" />
+          <InfoPopupDetailItem keyName="Author" value="Praveen" />
+        </div>
       </div>
     </div>
   );
