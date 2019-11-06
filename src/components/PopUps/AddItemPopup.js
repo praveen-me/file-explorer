8afc109c8b0eff667a3a40f57669595f3d9c3f5d
praveen-me <praveenkrsaini0@gmail.com>
2019-11-06 23:10:55 +0530

    Add overlay and toggle add Item Modal

--- src/components/PopUps/AddItemPopup.js
+++ src/components/PopUps/AddItemPopup.js
@@ -1,44 +1,48 @@
 import React from "react";
 import CloseBtn from "../utils/CloseBtn";
 
-const AddItemPopup = () => {
+const AddItemPopup = ({ closeModal }) => {
   return (
-    <div className="add_item modal">
-      <div className="modal__head-container">
-        <h3 className="modal-head__head">Create New </h3>
-        <CloseBtn />
-      </div>
-      <form className="add_item__form">
-        <div className="add_item__toggle">
-          <button className="add_item__toggle-btn btn">File</button>
-          <button className="add_item__toggle-btn btn add_item__toggle-btn--active">
-            Folder
+    <div className="overlay">
+      <div className="add_item modal">
+        <div className="modal__head-container">
+          <h3 className="modal-head__head">Create New </h3>
+          <button className="btn" onClick={closeModal}>
+            <CloseBtn />
           </button>
         </div>
-        <section className="add_item__details">
-          <input
-            type="text"
-            className="add_item__details-item"
-            name="name"
-            placeholder="Name"
-          />
-          <input
-            type="text"
-            className="add_item__details-item"
-            name="creator"
-            placeholder="Creator"
-          />
-          <input
-            type="text"
-            className="add_item__details-item"
-            name="Size"
-            placeholder="Size"
-          />
-        </section>
-        <button type="submit" className="add_item__submit btn">
-          Create
-        </button>
-      </form>
+        <form className="add_item__form">
+          <div className="add_item__toggle">
+            <button className="add_item__toggle-btn btn">File</button>
+            <button className="add_item__toggle-btn btn add_item__toggle-btn--active">
+              Folder
+            </button>
+          </div>
+          <section className="add_item__details">
+            <input
+              type="text"
+              className="add_item__details-item"
+              name="name"
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              className="add_item__details-item"
+              name="creator"
+              placeholder="Creator"
+            />
+            <input
+              type="text"
+              className="add_item__details-item"
+              name="Size"
+              placeholder="Size"
+            />
+          </section>
+          <button type="submit" className="add_item__submit btn">
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
