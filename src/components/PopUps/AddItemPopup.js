631978895add1fc2e11b67a25944db741719d319
praveen-me <praveenkrsaini0@gmail.com>
2019-11-06 16:26:36 +0530

    Add UI for adding item

---
+++ src/components/PopUps/AddItemPopup.js
@@ -0,0 +1,46 @@
+import React from "react";
+import CloseBtn from "../utils/CloseBtn";
+
+const AddItemPopup = () => {
+  return (
+    <div className="add_item">
+      <div className="add_item__head-container">
+        <h3 className="add_item__head">Create New </h3>
+        <CloseBtn />
+      </div>
+      <form className="add_item__form">
+        <div className="add_item__toggle">
+          <button className="add_item__toggle-btn btn">File</button>
+          <button className="add_item__toggle-btn btn add_item__toggle-btn--active">
+            Folder
+          </button>
+        </div>
+        <section className="add_item__details">
+          <input
+            type="text"
+            className="add_item__details-item"
+            name="name"
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            className="add_item__details-item"
+            name="creator"
+            placeholder="Creator"
+          />
+          <input
+            type="text"
+            className="add_item__details-item"
+            name="Size"
+            placeholder="Size"
+          />
+        </section>
+        <button type="submit" className="add_item__submit btn">
+          Create
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddItemPopup;
---
+++ src/components/utils/CloseBtn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Close } from "../../images/close.svg";
+
+const CloseBtn = () => {
+  return (
+    <div className="close-btn">
+      <Close />
+    </div>
+  );
+};
+
+export default CloseBtn;
