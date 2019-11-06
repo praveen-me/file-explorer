fc9202877ff32b8da460a64f1cf1df7a5d5c5ac4
praveen-me <praveenkrsaini0@gmail.com>
2019-11-07 02:15:23 +0530

    Create functionality of opening info and open folder from options

--- src/components/PopUps/OptionPopup.js
+++ src/components/PopUps/OptionPopup.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-const OptionPopup = () => {
+const OptionPopup = ({ open, toggleInfo, deleteItem, isInfoModalOpen }) => {
   return (
-    <div className="options">
-      <button className="options__btn btn">Open</button>
-      <button className="options__btn btn">Get Info</button>
+    <div
+      className="options"
+      style={{
+        zIndex: isInfoModalOpen ? 0 : 15
+      }}
+    >
+      <button className="options__btn btn" onClick={open}>
+        Open
+      </button>
+      <button className="options__btn btn" onClick={toggleInfo}>
+        Get Info
+      </button>
       <button className="options__btn options__btn--delete btn">Delete</button>
     </div>
   );
