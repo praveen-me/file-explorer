53899afb057d44aede2c48dd2cf0ae4abf846877
praveen-me <praveenkrsaini0@gmail.com>
2019-11-09 18:28:20 +0530

    Add error boundary

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 import SideBar from "./components/SideBar";
 import MainPanel from "./components/MainPanel";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const data = useSelector((state) => state);
-
   return (
-    <>
+    <ErrorBoundary>
       <SideBar data={data.explorer} />
       <MainPanel data={data} />
-    </>
+    </ErrorBoundary>
   );
 }
 
